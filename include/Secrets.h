1cf14ac78991043c425c5dca3de2a16fa19045ac
Brian Price <brian.price@fishtouch.co>
2021-02-03 20:07:45 +0000

    initial commit

---
+++ include/Secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Nebula1"
+#define SECRET_PASS "ironmaiden88"
+#define SECRET_SSID2  "Shabranso"
+#define SECRET_PASS2  "En*-T,<rT*.W@**6=/Gljv;qmR_-@1O9%zMYgBm~:l95=.31M+n0?McjS/<A4e7"
